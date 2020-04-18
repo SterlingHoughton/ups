ae3681a63ae2a620c0afd058738d2455a7f00ae9
sterlinghoughton@gmail.com <sterling.houghton@nike.com>
2020-04-18 16:52:51 -0700

    create GET handlers

--- packages/db_service/routes/get.js
+++ packages/db_service/routes/get.js
@@ -1,5 +1,49 @@
-module.exports = server => {
-  server.get('/', (req, res) => {
-    res.send('Hello from the database');
-  })
+const mongoose = require('mongoose');
+const Mail = mongoose.model('Mail');
+
+const pingHandler = (req, res) => {
+  res.send('Healthy')
+};
+
+const mailHandler = async (req, res) => {
+  let mails;
+  let error;
+
+  try {
+    mails = await Mail.find();
+  } catch {
+    error = err;
+  }
+
+  res.send({
+    message: 'Got response from DB',
+    service: 'DB Service',
+    status: 200,
+    payload: mails || error
+  });
+};
+
+const singleMailHandler = async ({ params: { id } }, res) => {
+  let mail;
+  let error;
+
+  try {
+    mail = await Mail.findOne({ _id: id })
+  } catch {
+    error = err
+  }
+
+  res.send({
+    message: 'Got response from DB!',
+    service: 'DB Service',
+    status: 200,
+    payload: mail || error
+  });
 }
+
+module.exports = server => {
+  server
+  .get('/', pingHandler)
+  .get('/mails', mailHandler)
+  .get('/mails/:id', singleMailHandler)
+};
